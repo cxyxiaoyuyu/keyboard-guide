ef33c5d514c891864050655b191bbc7d5c66f574
xiaoyu <923996381@qq.com>
2018-10-28 11:14:53 +0800

    function

--- index.js
+++ index.js
@@ -1,54 +1,83 @@
-var keys = [
-['q','w','e','r','t','y','u','i','o','p'],
-['a','s','d','f','g','h','j','k','l'],
-['z','x','c','v','b','n','m']
-]
-var hash = {'q': 'qq.com', 'w': 'weibo.com', 'e': 'ele.me', 'r': 'renren.com', 't': 'tianya.com', 'y': 'youtube.com', 'u': 'uc.com' , 'i': 'iqiyi.com', 'o': 'opera.com', 'p': undefined, 'a': 'acfun.tv', 's': 'sohu.com', 'z': 'zhihu.com', 'm': 'www.mcdonalds.com.cn'}
 
-var hashInLocalStorage = JSON.parse(localStorage.getItem('zzz') || 'null')
-console.log(hashInLocalStorage)
-if(hashInLocalStorage)
-    hash = hashInLocalStorage
+//1 initial Data
+keys = initialData().keys
+hash = initialData().hash
 
-for(var i=0;i<keys.length;i++){
-    div = document.createElement('div')
-    div.className = 'row'
-    main.appendChild(div)
-    for(var j=0;j<keys[i].length;j++){
-        kbd = document.createElement('kbd')
-        span = document.createElement('span')
-        span.textContent = keys[i][j]
-        span.className = 'text'
-        kbd.appendChild(span)
-        kbd.className = 'key'
-        button= document.createElement('button')
-        button.textContent = 'Edit'
-        button.id = keys[i][j]
-        button.onclick = function(ev){
-            id = ev.target.id
-            input_site = prompt('Give me a website')
-            hash[id] = input_site
-            console.log(ev.target.previousSibling)
-            ev.target.previousSibling.src= 'http://'+input_site+'/favicon.ico'
-            localStorage.setItem('zzz',JSON.stringify(hash))
-            console.log(localStorage)
+// 2 create Element
+generateElement()
 
-        }
-        img = document.createElement('img')
-        img.alt = ''
-        img.src = 'http://'+hash[keys[i][j]] + '/favicon.ico'
-        kbd.appendChild(img)
-        kbd.appendChild(button)
-        div.appendChild(kbd)
+// 3 add Listener
+addListener()
+
+
+
+//1 initial data
+function initialData(){
+    var keys = [
+        ['q','w','e','r','t','y','u','i','o','p'],
+        ['a','s','d','f','g','h','j','k','l'],
+        ['z','x','c','v','b','n','m']
+    ]
+    var hash = {'q': 'www.qq.com', 'w': 'weibo.com', 'e': 'ele.me', 'r': 'renren.com', 't': 'tianya.com', 'y': 'youtube.com', 'u': 'uc.com' , 'i': 'iqiyi.com', 'o': 'opera.com', 'p': undefined, 'a': 'acfun.tv', 's': 'www.sohu.com', 'z': 'zhihu.com', 'm': 'www.mcdonalds.com.cn'}
+    // get from localStorage
+    var hashInLocalStorage = JSON.parse(localStorage.getItem('zzz') || 'null')
+    if(hashInLocalStorage)
+        hash = hashInLocalStorage
+
+    return {
+        'keys': keys,
+        'hash': hash
     }
 }
 
+function createElement(tagName,attributes){
+    tag = document.createElement(tagName)
+    for(var key in attributes){
+        tag[key] = attributes[key]
+    }
+    return tag
+}
+// 2 create element
+function generateElement(){
+    for(var i=0;i<keys.length;i++){
+        div = createElement('div',{'className':'row'})
+        main.appendChild(div)
 
-document.onkeypress = function(ev){
-    key = ev.key
-    website = hash[key]
-    window.open('http://'+website)
+        for(var j=0;j<keys[i].length;j++){
+            span = createElement('span',{'textContent':keys[i][j],'className':'text'})
+            button = createElement('button',{'textContent':'Edit','id':keys[i][j]})
+            button.onclick = function(ev){
+                id = ev.target.id
+                input_site = prompt('Give me a website')
+                hash[id] = input_site
+                console.log(ev.target.previousSibling)
+                ev.target.previousSibling.src= 'http://'+input_site+'/favicon.ico'
+                localStorage.setItem('zzz',JSON.stringify(hash))
+                console.log(localStorage)
+
+            }
+
+            img = createElement('img',{'src':'http://'+hash[keys[i][j]]+'/favicon.ico'})
+            img.onerror = function(ev){
+                ev.target.src="./dot.png"
+            }
+
+            kbd = createElement('kbd',{'className':'key'})
+
+            kbd.appendChild(span)
+            kbd.appendChild(img)
+            kbd.appendChild(button)
+            div.appendChild(kbd)
+        }
+    }
 }
 
-
+// 3 add Listener
+function addListener(){
+    document.onkeypress = function(ev){
+        key = ev.key
+        website = hash[key]
+        window.open('http://'+website)
+    }
+}
 
